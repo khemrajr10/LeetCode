6a19d69c64b4ae27ff1689f65402ca19adcacc0f
Khemraj Rajput <khemrajrajput07@gmail.com>
2026-07-27 21:45:30 +0530

    Time: 3 ms (52.56%), Space: 14.4 MB (39.89%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void backtrack(vector<int>& candidates, int target, int index, vector<int>& temp) {
+        if(target == 0) {
+            ans.push_back(temp);
+            return;
+        }
+
+        if(target < 0 || index == candidates.size())
+            return;
+
+        temp.push_back(candidates[index]);
+        backtrack(candidates, target - candidates[index], index, temp);
+        temp.pop_back();
+
+        backtrack(candidates, target, index + 1, temp);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> temp;
+        backtrack(candidates, target, 0, temp);
+        return ans;
+    }
+};
