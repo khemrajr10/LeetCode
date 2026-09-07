605e534d0a84f284885225b3e54d559b175b61e3
Khemraj Rajput <khemrajrajput07@gmail.com>
2026-09-07 21:42:00 +0530

    Time: 0 ms (100%), Space: 20.7 MB (24.15%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,6 +3,22 @@ public:
     int singleNumber(vector<int>& nums) {
         int n= nums.size();
 
+        int xorr=0;
+
+        for(int i=0; i<n; i++){
+            xorr = xorr ^ nums[i];
+        }
+        return xorr;
+        
+        
+    }
+};
+
+
+/*
+
+int n= nums.size();
+
         unordered_map<int,int>freq;
 
         for(int i=0; i<n; i++){
@@ -15,8 +31,4 @@ public:
             }
         }
 
-        return 0;
-        
-        
-    }
-};
+*/
