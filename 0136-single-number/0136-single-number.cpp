f9f2c8b9a7c4c44bdecb04a0cd22cef996d5c927
Khemraj Rajput <khemrajrajput07@gmail.com>
2026-09-07 21:44:19 +0530

    Time: 0 ms (100%), Space: 20.5 MB (98.74%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -32,3 +32,24 @@ int n= nums.size();
         }
 
 */
+
+/*
+
+brute force approach
+
+for(int i=0; i<n; i++){
+int count=0
+int num=nums[i];
+
+for(int j=0; j<n; j++){
+if(nums[i]==num){
+count++;
+
+}
+}
+
+if(count ==1 ){
+return num;
+}
+}
+*/
