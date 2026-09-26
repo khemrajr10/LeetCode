d9adb0def0f789039095ee2eaf7b1dbccc035c37
Khemraj Rajput <khemrajrajput07@gmail.com>
2026-09-26 12:13:40 +0530

    Time: 0 ms (100%), Space: 263.4 MB (45.98%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,33 +1,63 @@
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+//         int n = nums.size();
+
+//         k = k % n;
+
+//         reverse(nums.begin(), nums.end());
+
+//         vector<int> a;
+//         vector<int> b;
+
+//         for(int i = 0; i < k; i++) {
+//             a.push_back(nums[i]);
+//         }
+
+//         for(int i = k; i < n; i++) {
+//             b.push_back(nums[i]);
+//         }
+
+//         reverse(a.begin(), a.end());
+//         reverse(b.begin(), b.end());
+
+        
+//         for(int i = 0; i < a.size(); i++) {
+//             nums[i] = a[i];
+//         }
+
+//         for(int i = 0; i < b.size(); i++) {
+//             nums[a.size() + i] = b[i];
+//         }
+//     }
+// };
+
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int n = nums.size();
+         int n= nums.size();
+         reverse(nums.begin(),nums.end());
 
         k = k % n;
 
-        reverse(nums.begin(), nums.end());
+        int s= 0;
+        int m = k-1 ;
+        int l = n-1;
 
-        vector<int> a;
-        vector<int> b;
-
-        for(int i = 0; i < k; i++) {
-            a.push_back(nums[i]);
+        while(s<m){
+            swap(nums[s],nums[m]);
+            s++;
+            m--;
         }
 
-        for(int i = k; i < n; i++) {
-            b.push_back(nums[i]);
+        while(k<l){
+            swap(nums[k],nums[l]);
+            k++;
+            l--;
         }
-
-        reverse(a.begin(), a.end());
-        reverse(b.begin(), b.end());
+      
 
         
-        for(int i = 0; i < a.size(); i++) {
-            nums[i] = a[i];
-        }
-
-        for(int i = 0; i < b.size(); i++) {
-            nums[a.size() + i] = b[i];
-        }
+      
     }
 };
